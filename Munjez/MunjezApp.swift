9717746823b131278d383ff4e1b1ae7b5f95cad8
Lujain <lujain.abh>
2023-10-09 11:43:05 +0300

    Initial Commit

---
+++ Munjez/MunjezApp.swift
@@ -0,0 +1,17 @@
+//
+//  MunjezApp.swift
+//  Munjez
+//
+//  Created by Lujain Abdullah Halabi Almeri on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct MunjezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
