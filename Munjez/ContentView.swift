bd0bfc21b9ed006e18083c18cd1be3ac3c812d61
Lujain <lujain.abh>
2023-10-11 12:44:13 +0300

    Second commit

--- Munjez/ContentView.swift
+++ Munjez/ContentView.swift
@@ -8,16 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var givenName: String = ""
+    @State private var isTapped = false
+    let recName = "testi"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.munjezBack).edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("أهلا ياصديقنا ! ساعدنا بالتعرف عليك ")
+                .font(.largeTitle)
+
+                TextField( " اسمك",  text: $givenName)
+                               .padding(.leading, 300)
+                               .foregroundColor(.gray)
+                               .font(.largeTitle)
+                               .frame(width: 673 , height: 95)
+                               .background(Color.gray.opacity(0.09))
+                               .cornerRadius(29)
+
+                HStack{
+                    ZStack{
+                        Rectangle().foregroundColor(Color.white).frame(maxWidth: 200, maxHeight: 200)
+                            .background(Color.white)
+                            .padding(0)
+                            .cornerRadius(20)
+                            .padding(.horizontal,15)
+                            Image("r")
+                            .id(recName)
+                      
+                        
+                        Text("\(recName)")
+                    }
+                    Text("Stake tokens and get rewards")
+                        .foregroundColor(.black)
+                        .frame(maxWidth: 200, maxHeight: 200)
+                        .background(Color.white)
+                        .padding(0)
+                        .cornerRadius(20)
+                        .padding(.horizontal,15)
+                    
+                    Text("Stake tokens and get rewards")
+                        .foregroundColor(.black)
+                        .frame(maxWidth: 200, maxHeight: 200)
+                        .background(Color.white)
+                        .padding(0)
+                        .cornerRadius(20)
+                        .padding(.all,15)
+                }
+                HStack{
+                    Text("Stake tokens and get rewards")
+                        .foregroundColor(.black)
+                        .frame(maxWidth: 200, maxHeight: 200)
+                        .background(Color.white)
+                        .padding(0)
+                        .cornerRadius(20)
+                        .padding(.all,15)
+                    
+                    Text("Stake tokens and get rewards")
+                        .foregroundColor(.black)
+                        .frame(maxWidth: 200, maxHeight: 200)
+                        .background(Color.white)
+                        .padding(0)
+                        .cornerRadius(20)
+                        .padding(.all,15)
+                    
+                    Text("last")
+                        .foregroundColor(.black)
+                        .frame(maxWidth: 200, maxHeight: 200)
+                        .background(Color.white)
+                        .padding(0)
+                        .cornerRadius(20)
+                        .padding(.all,15)
+                        /*.onTapGesture {
+                            print("Image tapped!")
+                        }*/
+                    
+                }
+              
+            }
         }
-        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
--- Munjez/FinalPage.swift
+++ Munjez/FinalPage.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct FinalPage: View {
+    @State private var isRotating = 0.0
+    @State private var scale = 1.0
+    var body: some View {
+        ZStack {
+            Color(.munjezBack).edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("مبارك يا أحمد ").font(.largeTitle).bold().frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .center).foregroundColor(.munjezYellow)
+                    .padding(.vertical, -320)
+                Text("لقد كان أسبوعًا حافلًا بالإنجاز!").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).bold().frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .center).foregroundColor(.munjezBlue)
+                    .padding(.vertical, -280)
+            }
+             Spacer()
+             Spacer()
+           
+            Image("fires")
+                .resizable()
+                .frame(width: 400, height: 500)
+                .scaledToFit()
+                .scaleEffect(scale)
+                .onAppear() {
+                    scale += 0.5
+                                }
+                .animation(.spring(response: 0.7, dampingFraction: 0.1), value: scale)
+            
+            Image("celeberation").resizable().frame(width: 700, height: 700)
+                            }
+        }
+    }
+
+
+#Preview {
+    FinalPage()
+}
