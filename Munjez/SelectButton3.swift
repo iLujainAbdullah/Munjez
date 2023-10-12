03672ccec298130d058f991d9860d58e4142ff37
DevAhlam <Ahlamalmutiri.dev@gmail.com>
2023-10-12 13:33:55 +0300

    commit

--- Munjez/SelectButton3.swift
+++ Munjez/SelectButton3.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct SelectButton3: View {
+    
+    @Binding var isSelected : Bool
+    @State  var image2 :Image
+    @State var text : String
+    @State var color :Color
+    var body: some View {
+        
+      
+        ZStack{
+            Rectangle()
+                .frame(width: 260,height: 260)
+                .foregroundColor(isSelected ? color : .white)
+                .cornerRadius(45)
+                .shadow(radius:10)
+           
+            VStack{
+               image2
+                    .resizable()
+                    .frame(width: 200 ,height: 200)
+               
+                Text(text)
+                    .foregroundColor(Color(.munjezYellow))
+                    .font(.largeTitle)
+                
+            }}
+            
+            
+        }}
+
+    
+
+
+struct SelectButton3_Previews : PreviewProvider {
+    static var previews : some View {
+
+        SelectButton3(isSelected: .constant(false) ,image2:Image("custom-image"),text : "option", color : .orange)
+       
+    }}
+
--- Munjez/SelectTask.swift
+++ Munjez/SelectTask.swift
@@ -6,3 +6,132 @@
 //
 
 import Foundation
+import Foundation
+import SwiftUI
+
+struct SelectTask: View{
+    @State private var isSelected1 = false
+    @State private var isSelected2 = false
+    @State private var isSelected3 = false
+    @State private var isSelected4 = false
+    @State private var isSelected5 = false
+    @State private var isSelected6 = false
+    var body: some View {
+        
+       
+            
+            ZStack{
+                Color(.munjezBack) .edgesIgnoringSafeArea(.all)
+                
+                ScrollView{
+                    VStack{
+                        
+                        HStack{
+                            Text("ساعدنا بالتعرف عليك")
+                                .font(.largeTitle)
+                            Text("أهلاً ياصديقنا !")
+                                .foregroundColor(Color(.munjezYellow))
+                                .font(.largeTitle)
+                            
+                        } .padding()
+                        
+                        HStack{
+                            
+                            
+                            SelectButton3(
+                                isSelected: $isSelected1,
+                                image2: Image("eat") ,
+                                text: "تناول الطعام "
+                                , color: .accentColor)
+                            .onTapGesture {
+                                isSelected1.toggle()
+                            }
+                            .padding()
+                            
+                            SelectButton3(
+                                isSelected: $isSelected2,
+                                image2: Image("teeth") ,
+                                text: "تفريش الاسنان "
+                                , color: .accentColor)
+                            .onTapGesture {
+                                isSelected2.toggle()
+                            }
+                            .padding()
+                            SelectButton3(
+                                isSelected: $isSelected3,
+                                image2: Image("sun") ,
+                                text: "الاستيقاظ مبكرا "
+                                , color: .accentColor)
+                            .onTapGesture {
+                                isSelected3.toggle()
+                            }.padding()
+                            
+                        }
+                        
+                        
+                        
+                        
+                        
+                       
+                        
+                        
+                        HStack{
+                            
+                            
+                            SelectButton3(
+                                isSelected: $isSelected4,
+                                image2: Image("sleep") ,
+                                text: "وقت النوم"
+                                , color: .accentColor)
+                            .onTapGesture {
+                                isSelected4.toggle()
+                            }
+                            .padding()
+                            
+                            SelectButton3(
+                                isSelected: $isSelected5,
+                                image2: Image("play") ,
+                                text: "وقت اللعب"
+                                , color: .accentColor)
+                            .onTapGesture {
+                                isSelected5.toggle()
+                                
+                                
+                            }.padding()
+                            SelectButton3(
+                                isSelected: $isSelected6,
+                                image2: Image("homework") ,
+                                text: "حل الواجبات"
+                                , color: .accentColor)
+                            .onTapGesture {
+                                isSelected6.toggle()
+                                
+                                
+                            }.padding()
+                            
+                            
+                            
+                            
+                        }
+                        
+                    }
+                    
+               
+                }
+            
+            
+        }
+        
+        
+    }
+}
+        
+        
+        
+    
+
+   
+#Preview {
+    
+    SelectTask()
+}
