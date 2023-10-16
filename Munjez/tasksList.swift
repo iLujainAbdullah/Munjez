f1afe91e0444252beb4deeae6cd479190496a0ee
iLujainAbdullah <lujain.abh@gmail.com>
2023-10-16 12:16:15 +0300

    COMMIT!!!!!

---
+++ Munjez/tasksList.swift
@@ -0,0 +1,18 @@
+//
+//  tasksList.swift
+//  Munjez
+//
+//  Created by Lujain Abdullah Halabi Almeri on 01/04/1445 AH.
+//
+
+import SwiftUI
+
+struct tasksList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    tasksList()
+}
