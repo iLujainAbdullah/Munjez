3aa3bb681ca06a0562014e9e286aeda84de2bc20
reemalhalafi <alhalafireem@gmail.com>
2023-10-17 11:00:14 +0300

    Commit

---
+++ Munjez/mImage.swift
@@ -0,0 +1,27 @@
+//
+//  map2.swift
+//  Munjez
+//
+//  Created by Reem Alhalafi on 02/04/1445 AH.
+//
+
+import Foundation
+import SwiftUI
+
+struct mImage: View {
+    @State var img: Image
+    var body: some View {
+        img
+            .resizable()
+            .frame(width: 100, height: 100)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.munjezCircle, lineWidth:5))
+            .padding(-370)
+            .padding(.leading,70)
+    }
+}
+struct mIage_Previews: PreviewProvider{
+    static var previews: some View{
+        mImage(img: Image("custom-image"))
+    }
+}
---
+++ Munjez/map.swift
@@ -0,0 +1,18 @@
+//
+//  map.swift
+//  Munjez
+//
+//  Created by Reem Alhalafi on 02/04/1445 AH.
+//
+
+import SwiftUI
+
+struct map: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    map()
+}
