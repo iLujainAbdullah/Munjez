3933a4ad45654e8a0547db9971d5cf7bccd3d904
iLujainAbdullah <lujain.abh@gmail.com>
2023-10-18 08:56:37 +0300

    Final commit

---
+++ Munjez/EndDay.swift
@@ -0,0 +1,18 @@
+//
+//  EndDay.swift
+//  Munjez
+//
+//  Created by Lujain Abdullah Halabi Almeri on 02/04/1445 AH.
+//
+
+import SwiftUI
+
+struct EndDay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EndDay()
+}
