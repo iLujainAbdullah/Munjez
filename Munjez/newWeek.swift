6187f14aabd4166fd0f5792dba8bd3cd47b5afdd
amjaadalzaidi <amjaadalzaidi.o@gmail.com>
2023-11-28 12:24:20 +0300

    co

---
+++ Munjez/newWeek.swift
@@ -0,0 +1,18 @@
+//
+//  newWeek.swift
+//  Munjez
+//
+//  Created by amjaad on 13/05/1445 AH.
+//
+
+import SwiftUI
+
+struct newWeek: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    newWeek()
+}
